aff3fb36c266ed8fca882554b3bfd86d05366f44
derickddo <dansoderrick80@yahoo.com>
2025-07-10 11:37:41 +0000

    refactor: Update Vite config to load environment variables within a function for improved context handling

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  env: loadEnv(mode, process.cwd()),
-  
-  plugins: [react(), tailwindcss()],
-  define: {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
       'import.meta.env.VITE_BACKEND_URL': JSON.stringify(env.VITE_BACKEND_URL),
       'import.meta.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(env.VITE_GOOGLE_CLIENT_ID),
       'import.meta.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify(env.VITE_GOOGLE_MAPS_API_KEY),
       'import.meta.env.VITE_PAYSTACK_PUBLIC_KEY': JSON.stringify(env.VITE_PAYSTACK_PUBLIC_KEY),
     },
-})
+  };
+});
