d03417f448294bc174026877b9155b2bcb0a36ca
derickddo <dansoderrick80@yahoo.com>
2026-02-25 16:44:43 +0000

    updated axios instance

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://2066f3f6e65aea22898f562c9c5b6099@o4510912763723776.ingest.us.sentry.io/4510934836183040",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.,
+  // Enable logs to be sent to Sentry
+  enableLogs: true
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
